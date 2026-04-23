f89a6c0ff55c667c37c179734b650502cf54916f
pro <pro@pros-MacBook-Pro.local>
2026-04-23 07:47:43 +0300

    started python variables

---
+++ main.py
@@ -0,0 +1,20 @@
+
+# my first python program
+# print("I like dogs!")
+# print("Hello, world!")
+
+# Vriables = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+first_name = "Novic"
+food = "chapati"
+email = "novikmuliro5@gmail.com"
+degree = "Computer Science"
+
+print(first_name)
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+print(f"You did {degree} in campus")
+
